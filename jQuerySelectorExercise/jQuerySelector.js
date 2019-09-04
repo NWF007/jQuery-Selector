a0b1232eacf2dfca31f11d6e4749da3625c94622
NWF007 <53278049+NWF007@users.noreply.github.com>
2019-09-04 13:09:53 +0200

    Add files via upload

---
+++ jQuerySelectorExercise/jQuerySelector.js
@@ -0,0 +1,11 @@
+//all the divs
+$("div").css("backgroundColor", "purple");
+
+//divs with class highlight
+$(".highlight").css("width", "200px");
+
+//div with id third
+$("#third").css("border", "2px solid orange");
+
+//only the first div
+$("div:first-of-type").css("color", "pink");
